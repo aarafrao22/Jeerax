01085812db7552935b5911b85c43192af05f2980
Rao Aaraf Younus <aarafrao22@gmail.com>
2023-01-28 16:11:29 +0500

    Two Screens Done

--- app/src/main/java/com/aarafrao/jeerax/HomeActivity.java
+++ app/src/main/java/com/aarafrao/jeerax/HomeActivity.java
@@ -2,13 +2,26 @@ package com.aarafrao.jeerax;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.aarafrao.jeerax.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.floatingActionButton.setOnClickListener(v -> {
+            startActivity(new Intent(HomeActivity.this, AddPasswordActivity.class));
+        });
+
     }
 }
