1febf3cdd21aef0f16d43de8e1b6c47faeec92c6
Rao Aaraf Younus <aarafrao22@gmail.com>
2023-01-29 00:01:00 +0500

    RecyclerViewAdded

--- app/src/main/java/com/aarafrao/jeerax/HomeActivity.java
+++ app/src/main/java/com/aarafrao/jeerax/HomeActivity.java
@@ -1,12 +1,16 @@
 package com.aarafrao.jeerax;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.aarafrao.jeerax.databinding.ActivityHomeBinding;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -19,9 +23,18 @@ public class HomeActivity extends AppCompatActivity {
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.floatingActionButton.setOnClickListener(v -> {
-            startActivity(new Intent(HomeActivity.this, AddPasswordActivity.class));
-        });
+        binding.floatingActionButton.setOnClickListener(v -> startActivity(new Intent(HomeActivity.this, AddPasswordActivity.class)));
+
+        ArrayList<ItemModel> rvList = new ArrayList<>();
+        rvList.add(new ItemModel("Facebook", R.drawable.ic_launcher_foreground));
+        rvList.add(new ItemModel("Google", R.drawable.ic_launcher_foreground));
+        rvList.add(new ItemModel("YT", R.drawable.ic_launcher_foreground));
+        rvList.add(new ItemModel("Facebook", R.drawable.ic_launcher_foreground));
+        rvList.add(new ItemModel("Whatsapp", R.drawable.ic_launcher_foreground));
+        RvAdapter rvAdapter = new RvAdapter(rvList, getApplicationContext());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.recyclerView.setLayoutManager(layoutManager);
+        binding.recyclerView.setAdapter(rvAdapter);
 
     }
 }
