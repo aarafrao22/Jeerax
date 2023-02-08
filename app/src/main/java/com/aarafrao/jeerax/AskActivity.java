94f32a48af7a26f0656544210e65a4744a98dda5
aaraf.younus <aarafrao22@gmail.com>
2023-02-08 10:00:45 +0500

    AddedBiometric

--- app/src/main/java/com/aarafrao/jeerax/AskActivity.java
+++ app/src/main/java/com/aarafrao/jeerax/AskActivity.java
@@ -1,6 +1,8 @@
 package com.aarafrao.jeerax;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,9 +10,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.Toast;
 
 import com.aarafrao.jeerax.databinding.ActivityAskBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.concurrent.Executor;
 
 
 public class AskActivity extends AppCompatActivity {
@@ -35,10 +40,7 @@ public class AskActivity extends AppCompatActivity {
                     startActivity(new Intent(getApplicationContext(), HomeActivity.class));
 
             } else {
-                startActivity(
-                        new Intent(getApplicationContext(),
-                                LoginActivity.class)
-                );
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
 
             }
         });
@@ -56,9 +58,40 @@ public class AskActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
-
-
             }
         });
+
+
+        binding.imgFingerprint.setOnClickListener(v -> authenticate());
+
     }
+
+    private void authenticate() {
+        BiometricPrompt biometricPrompt = new BiometricPrompt(AskActivity.this, runnable -> {
+        }, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(), "Authentication error: " + errString, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(), "Authentication succeeded!", Toast.LENGTH_SHORT).show();
+
+                startActivity(new Intent(AskActivity.this, HomeActivity.class));
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Authentication failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder().setTitle("Biometric login for my app").setSubtitle("Log in using your biometric credential").setNegativeButtonText("Use account password").build();
+        biometricPrompt.authenticate(promptInfo);
+    }
+
 }
